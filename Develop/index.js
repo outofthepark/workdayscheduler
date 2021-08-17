66f4ddc7c93a5850f03f6e5ea8539eeb702ef7f6
Allison Harden <harden.allison@gmail.com>
2021-08-17 10:45:10 -0600

    Updated to store events in local storage and change color.

---
+++ Develop/index.js
@@ -0,0 +1,51 @@
+
+updatePage();
+
+function updatePage(){
+    var date = dayjs().format('MMMM D YYYY');
+    var today = $("#today");
+    today.text(date);
+    //If it's a new day, delete the schedule from the previous day
+    if(localStorage.today){
+        if(localStorage.today != date){
+            localStorage.clear;
+            localStorage.setItem('today', date);
+        }
+    }else{
+        localStorage.setItem('today', date);
+    }
+    var currentHour = dayjs().hour();
+
+
+    $( '.eventField' ).each(function( index ) {
+        var hourValue = index + 9;
+        var textArea = $( this ).children().first();
+        textArea.prop('id', hourValue);
+    
+        //Loads stored event infromation into the text area
+        if(localStorage[hourValue])
+        {
+            textArea.val(localStorage[hourValue]);
+        }
+
+        //Sets past hours to gray, current hour to red, and future hours to green. 
+        if(hourValue < currentHour){
+            $( this ).css('background-color', 'gray');
+        }else if( hourValue > currentHour ){
+            $( this ).css('background-color', 'green');
+        }else if( hourValue == currentHour ){
+            $( this ).css('background-color', 'red');
+        }
+    });
+
+    //Sets a timer to update again when the hour changes.
+    var currentMinutes = dayjs().minute();
+    var timeTillNextHour = (60 - currentMinutes)*1000;
+    setTimeout( updatePage, timeTillNextHour)
+}
+
+$('.saveBtn').click( function(){
+    var eventField = $(this).parent().prev().children().first();
+    var hourValueOfEventField = eventField.prop('id');
+    localStorage.setItem(hourValueOfEventField, eventField.val());
+});
